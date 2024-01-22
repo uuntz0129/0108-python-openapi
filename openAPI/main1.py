0252c939ad2087f900e2c2b18de0425f77933bdc
lily <uuntz0129@gmail.com>
2024-01-22 11:34:08 +0800

    modify

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -12,3 +12,24 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+#@app.get("/items/{item_id}")
+#async def get_item(item_id):
+    #print(f"使用者輸入了:{item_id}")
+    #return {"item_id":item_id}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id:int):
+    print(f"使用者輸入了:{item_id}")
+    return {"item_id":item_id}
+
+@app.get("/items/{date}/{celsius}")
+async def get_item(date:str,celsius:float):
+    print(f"日期:{date}")
+    print(f"溫度:{celsius}")
+    return {"日期":date,"攝氏溫度":celsius}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]

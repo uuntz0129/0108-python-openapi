ecc089fd7a99387db1c8539ac5690d1f0bb4653e
lily <uuntz0129@gmail.com>
2024-01-24 15:08:24 +0800

    modify

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -36,10 +36,24 @@ async def read_item(skip: int = 0, limit: int = 10):
 
 @app.get("/pico_w/{date}")
 async def read_item(date:str ,address:str,celsius:float,light:float):
-    print(f"日期:{date}")
-    print(f"位置:{address}")
-    print(f"攝氏:{celsius}")
-    print(f"光線:{light}")
+    #print(f"日期:{date}")
+    redis_conn.rpush('pico_w:date',date)
+    #print(f"位置:{address}")
+    redis_conn.hset('pico_w:address',mapping={date:address})
+    #print(f"攝氏:{celsius}")
+    redis_conn.hset('pico_w:temperature',mapping={date:celsius})
+    #print(f"光線:{light}")
+    redis_conn.hset('pico_w:light',mapping={date:light})
+
+    date_get = redis_conn.lrange('pico_w:date',-1,-1)[0].decode()
+    address_get = redis_conn.hget('pico_w:address',date_get).decode()
+    temperature_get = redis_conn.hget('pico_w:temperature',date_get).decode()
+    light_get = redis_conn.hget('pico_w:light',date_get).decode()
+    print(date_get)
+    print(address_get)
+    print(temperature_get)
+    print(light_get)
+    
     return {"狀態":"儲存成功"}
 
 @app.get("/pico_w/{date}")

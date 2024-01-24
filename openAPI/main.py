df2a3e997d3d251cf7eb6f26b6355877771ee736
lily <uuntz0129@gmail.com>
2024-01-24 16:41:51 +0800

    modify

--- openAPI/main.py
+++ openAPI/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 import redis
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
+
 load_dotenv()
 redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 
@@ -11,27 +13,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    counter = redis_conn.incr('test:increment',1)
+    return {"Counter": counter}
 
-@app.get("/items/{item_id}")
-async def get_item(item_id:int):
-    print(f"使用者輸入了:{item_id}")
-    return {"item_id":item_id}
-
-@app.get("/items/{date}/{celsius}")
-async def get_item(date:str,celsius:float):
-    print(f"日期:{date}")
-    print(f"溫度:{celsius}")
-    return {"日期":date,"攝氏溫度":celsius}
-
-
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-
+@app.get("/counter/{c}")
+def counter(c:int):
+    counter = redis_conn.incr('test:increment',c)
+    return {"Counter": counter}
 
 
 @app.get("/pico_w/{date}")
@@ -44,21 +32,26 @@ async def read_item(date:str ,address:str,celsius:float,light:float):
     redis_conn.hset('pico_w:temperature',mapping={date:celsius})
     #print(f"光線:{light}")
     redis_conn.hset('pico_w:light',mapping={date:light})
+    return {"狀態":"儲存成功"}
 
-    date_get = redis_conn.lrange('pico_w:date',-1,-1)[0].decode()
-    address_get = redis_conn.hget('pico_w:address',date_get).decode()
-    temperature_get = redis_conn.hget('pico_w:temperature',date_get).decode()
-    light_get = redis_conn.hget('pico_w:light',date_get).decode()
-    print(date_get)
-    print(address_get)
-    print(temperature_get)
-    print(light_get)
+
+class Pico_w(BaseModel):
+    date:str
+    address:str
+    temperature:float
+    light:float
+
+@app.get("/pico_w/")
+async def read_item(count:int=1):
+    date_list = redis_conn.lrange('pico_w:date',-count,-1)
+    dates = [date.decode() for date in date_list]
+    all_Data:[Pico_w] = []
+    for date in dates:
+        address_get = redis_conn.hget('pico_w:address',date).decode()
+        temperature_get = redis_conn.hget('pico_w:temperature',date).decode()
+        light_get = redis_conn.hget('pico_w:light',date).decode()
+        item = Pico_w(date=date,address=address_get,temperature=float(temperature_get),light=float(light_get))
+        all_Data.append(item)
+
     
-    return {"狀態":"儲存成功"}
-
-@app.get("/pico_w/{date}")
-async def read_item(date:str ,address:str,celsius:float=0.0):
-    print(f"日期:{date}")
-    print(f"位置:{address}")
-    print(f"攝氏:{celsius}")
-    return {"狀態":"儲存成功"}
+    return all_Data
